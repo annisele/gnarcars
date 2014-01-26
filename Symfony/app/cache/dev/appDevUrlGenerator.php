e23e0fd9a5a351bbfa86661219e3e796587135f6
fitzgerlard <yiwen@usc.edu>
2014-01-26 11:34:24 -0800

    l

--- Symfony/app/cache/dev/appDevUrlGenerator.php
+++ Symfony/app/cache/dev/appDevUrlGenerator.php
@@ -38,6 +38,7 @@ class appDevUrlGenerator extends Symfony\Component\Routing\Generator\UrlGenerato
         'join_car' => array (  0 =>   array (  ),  1 =>   array (    '_controller' => 'Hacktech\\GnarCarBundle\\Controller\\DefaultController::formAction',  ),  2 =>   array (  ),  3 =>   array (    0 =>     array (      0 => 'text',      1 => '/join_car/',    ),  ),  4 =>   array (  ),),
         'register' => array (  0 =>   array (  ),  1 =>   array (    '_controller' => 'Hacktech\\GnarCarBundle\\Controller\\DefaultController::registerAction',  ),  2 =>   array (  ),  3 =>   array (    0 =>     array (      0 => 'text',      1 => '/sign_in/reg',    ),  ),  4 =>   array (  ),),
         'logging_in' => array (  0 =>   array (  ),  1 =>   array (    '_controller' => 'Hacktech\\GnarCarBundle\\Controller\\DefaultController::logging_inAction',  ),  2 =>   array (  ),  3 =>   array (    0 =>     array (      0 => 'text',      1 => '/sign_in/login',    ),  ),  4 =>   array (  ),),
+        'confirmation' => array (  0 =>   array (  ),  1 =>   array (    '_controller' => 'Hacktech\\GnarCarBundle\\Controller\\DefaultController::thank_youAction',  ),  2 =>   array (  ),  3 =>   array (    0 =>     array (      0 => 'text',      1 => '/confirmation/',    ),  ),  4 =>   array (  ),),
     );
 
     /**
